ba50c338b8d58a13d0350a4956ab06d817e902cf
Ayush Verma <106526797+CrimsonREwind@users.noreply.github.com>
2024-03-05 18:01:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ image_input = st.write("enter input image name ")
 video_input = st.write("enter input video name ")
 video_output = st.write("enter output video name ")
 
-apic ="/content/gdrive/MyDrive/roop/input/"+image_input
-bpic ="/content/gdrive/MyDrive/roop/input/"+video_input
-cpic ="/content/gdrive/MyDrive/roop/output/"+video_output
+apic ="/content/gdrive/MyDrive/roopFORK/input/"+image_input
+bpic ="/content/gdrive/MyDrive/roopFORK/input/"+video_input
+cpic ="/content/gdrive/MyDrive/roopFORK/output/"+video_output
 
-new_directory = '/content/gdrive/MyDrive/roop'
+new_directory = '/content/gdrive/MyDrive/roopFORK'
 
 os.chdir(new_directory)
 
@@ -25,13 +25,3 @@ if st.button('Run script'):
     else:
         arguments = ['--keep-frames', '--keep-fps', '--execution-provider', 'cuda', '--frame-processor', 'face_swapper']
         subprocess.run(['python', script_name, f'-s {apic} -t {bpic} -o {cpic}'] + arguments)
-
-
-
-
-
-
-
-
-"""!python run.py -s $apic -t $bpic -o $cpic --keep-frames --keep-fps --execution-provider cuda --frame-processor face_swapper face_enhancer
-inf('\u2714 Execution Finished','success', '50px')"""
